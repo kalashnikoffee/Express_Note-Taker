36189e503bb1198c4d1eaaefa465623911a380b6
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-13 21:32:55 -0800

    path to notes and index assembly

--- server.js
+++ server.js
@@ -6,3 +6,36 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const dbPath = path.join(__dirname, "db", "db.json");
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+app.get("/api/notes", async function(req, res) {
+    try {
+      const notes = getNotes();
+      return res.send(notes);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+app.post("/api/notes", function(req, res) {
+    let notes = getNotes();
+    let ids = getIdNums();
+    let newNote = req.body;
+    for (let i = 1; i <= Object.keys(ids).length + 1; i++) {
+      if (!ids[i]) {
+        newNote.id = i;
+        ids[i] = true;
+        break;
+      }
+    }
+    notes.push(newNote);
+    writeFileAsync(dbPath, JSON.stringify(notes));
+    res.send(notes);
+  });

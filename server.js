a6cc72ea0e6ca62194c8b16b18fa5d995f448713
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-13 23:43:03 -0800

    more server.js assembly

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+//require express
 const express = require("express");
+//require path
 const path = require("path");
+//require fs
 const fs = require("fs");
+//reuqire util
 const util = require("util");
+//promisify fs.writefile
 const writeFileAsync = util.promisify(fs.writeFile);
+//directery path
 const dbPath = path.join(__dirname, "db", "db.json");
+//define express
 const app = express();
+//define PORT
 const PORT = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,6 +33,7 @@ app.get("/api/notes", async function(req, res) {
       console.log(err);
     }
   });
+//
 app.post("/api/notes", function(req, res) {
     let notes = getNotes();
     let ids = getIdNums();
@@ -39,6 +49,7 @@ app.post("/api/notes", function(req, res) {
     writeFileAsync(dbPath, JSON.stringify(notes));
     res.send(notes);
   });
+  //
   app.delete("/api/notes/:id", function(req, res) {
     let delId = parseInt(req.params.id);
     let notes = getNotes();
@@ -50,4 +61,32 @@ app.post("/api/notes", function(req, res) {
     }
     writeFileAsync(dbPath, JSON.stringify(notes));
     res.end();
+  });
+  //
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+
+  function getNotes() {
+    let notes = fs.readFileSync(dbPath, "utf8");
+    if (!notes) {
+      notes = [];
+    } else {
+      notes = JSON.parse(notes);
+    }
+    return notes;
+  }
+  
+  function getIdNums() {
+    let notes = getNotes();
+    let ids = {};
+    for (let i = 0; i < notes.length; i++) {
+      let id = notes[i].id;
+      ids[id] = true;
+    }
+    return ids;
+  }
+  
+  app.listen(PORT, function() {
+    console.log("App listening on PORT http://localhost:" + PORT);
   });

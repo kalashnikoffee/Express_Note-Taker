82a8d585b9c38171c091c19a6291c743180c24f4
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-14 00:07:56 -0800

    javascript for webpage functionality

---
+++ public/assets/js/index.js
@@ -0,0 +1,29 @@
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
+// activeNote is used to keep track of the note in the textarea
+var activeNote = {};
+// A function for getting all notes from the db
+var getNotes = function() {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET"
+  });
+};
+// A function for saving a note to the db
+var saveNote = function(note) {
+  return $.ajax({
+    url: "/api/notes",
+    data: note,
+    method: "POST"
+  });
+};
+// A function for deleting a note from the db
+var deleteNote = function(id) {
+  return $.ajax({
+    url: "api/notes/" + id,
+    method: "DELETE"
+  });
+};
